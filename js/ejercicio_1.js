f3c369daddb5bf2eda806ae6124f3d629f766fcc
sebastiamg <sebasortiz112004@gmail.com>
2022-02-23 08:52:11 -0500

    Ejercicio 1

---
+++ js/ejercicio_1.js
@@ -0,0 +1,7 @@
+document.getElementById("tipo1").innerHTML = "Este es el tipo 1 de Output";
+
+document.write("Este es el tipo 2: document.write, siempre se pone al último");
+
+window.alert("Este es el tipo 3: window.alert()");
+
+console.log("Este es el tipo 4: console.log()");
