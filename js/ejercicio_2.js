9ea91cbe2ec96ba6903295f263627f25bc7b4b2c
Sebastiamg <sebasortiz112004@gmail.com>
2022-03-02 12:51:21 -0500

    Actualizacion interfaz 2

--- js/ejercicio_2.js
+++ js/ejercicio_2.js
@@ -1,19 +1,6 @@
-let nota1, nota2, nota3, promedio
-nota1 = Number(prompt("Ingrese la nota 1"));
-nota2 = Number(prompt("Ingrese la nota 2"));
-nota3 = Number(prompt("Ingrese la nota 3"));
+const prompt = require("prompt-sync")();
 
-promedio = (nota1 + nota2 + nota3)/3
+var nombre = prompt("Nombre: ")
+var apellido = prompt("Apellido: ");
 
-document.getElementById("N1").innerHTML = "APROBADO";
-document.getElementById("N2").innerHTML = "APROBADO";
-document.getElementById("N3").innerHTML = "APROBADO";
-document.getElementById("PROM").innerHTML = "APROBADO";
-
-
-if (promedio >= 9.65) {
-    document.getElementById("N1, N2, N3").innerHTML = "APROBADO";
-} else {
-    document.getElementById("salida").innerHTML = "REPROBADO";
-    
-}
+console.log("Tu nombre completo es",nombre,apellido);
